58ad6747c22d5dd3a53877a153c299f83d7148a4
quan <lehoangquan@gmail.com>
2022-09-19 21:21:48 +0700

    first commit

---
+++ frontEnd/src/containers/Auth/Login.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import * as actions from "../../store/actions";
+
+import "./Login.scss";
+import { FormattedMessage } from "react-intl";
+import { handleLoginApi } from "../../services/userService";
+import { userLoginSuccess } from "../../store/actions";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      errMessage: "",
+    };
+  }
+
+  handleOnChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  handleOnChangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  handleLogin = async () => {
+    this.setState({
+      errMessage: "",
+    });
+    try {
+      let data = await handleLoginApi(this.state.username, this.state.password);
+      if (data && data.errCode !== 0) {
+        this.setState({
+          errMessage: data.message,
+        });
+      }
+      if (data && data.errCode === 0) {
+        this.props.userLoginSuccess(data.user);
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.data) {
+          this.setState({
+            errMessage: error.response.data.message,
+          });
+        }
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content">
+            <div className="col-12 text-login">Login</div>
+            <div className="col-12 form-group">
+              <label>Username:</label>
+              <input
+                type="text"
+                className="form-control login-input"
+                value={this.state.username}
+                onChange={(event) => this.handleOnChangeUsername(event)}
+              />
+            </div>
+            <div className="col-12 form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                className="form-control login-input"
+                value={this.state.password}
+                onChange={(event) => this.handleOnChangePassword(event)}
+              />
+            </div>
+            <div className="col-12" style={{ color: "red" }}>
+              {this.state.errMessage}
+            </div>
+            <div className="col-12">
+              <button
+                className="btn-login"
+                onClick={() => {
+                  this.handleLogin();
+                }}
+              >
+                Login
+              </button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password">Forgot your password?</span>
+            </div>
+            <div className="col-12">
+              <span className="text-center">Or Login with:</span>
+            </div>
+            <div className="col-12 social-login"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+
+    // userLoginFail: () => dispatch(actions.userLoginFail()),
+    userLoginSuccess: (userInfo) =>
+      dispatch(actions.userLoginSuccess(userInfo)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
---
+++ frontEnd/src/containers/System/UserManage.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
+class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
+
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
+    }
+  }
+
+  handleAddNewUser = () => {
+    alert("click me");
+  };
+
+  render() {
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className="users-container">
+        <div className="title text-center">Manage users with Quan</div>
+        <div className="mx-1">
+          <button
+            className="btn btn-primary px-3"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i className="fas fa-plus"></i> Add new user
+          </button>
+        </div>
+        <div className="users-table mt-4 mx-3">
+          <table id="customers">
+            <tbody>
+              <tr>
+                <th>Email</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Address</th>
+                <th>Actions</th>
+              </tr>
+
+              {this.state.arrUsers &&
+                this.state.arrUsers.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.email}</td>
+                      <td>{item.firstName}</td>
+                      <td>{item.lastName}</td>
+                      <td>{item.address}</td>
+                      <td>
+                        <button className="btn-edit">
+                          <i className="fas fa-pencil-alt"></i>
+                        </button>
+                        <button className="btn-delete">
+                          <i className="fas fa-trash"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
